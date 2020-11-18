7ea2c2edbd4ad706090b480ee32e755f6e103a8b
Dirta Putra Anggara <dirtaputraanggara@gmail.com>
2020-11-18 13:47:51 +0700

    Add api service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('hello-world')
+  getHelloWorld() {
+    return this.appService.getHelloWorld();
+  }
+
+  @Get('custom-hello')
+  getCustomHello(@Query('q') q: string) {
+    return this.appService.getCustomHello(q);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, Inject } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+
+interface HelloWorldService {
+  getHelloWorld(data: any): Observable<any>;
+}
+
+interface CustomHelloService {
+  getCustomHello(data: any): Observable<any>;
+}
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleInit {
+  private helloWorldService: HelloWorldService;
+  private customHelloService: CustomHelloService;
+
+  constructor(
+    @Inject('HELLO_WORLD_SERVICE') private helloWorldClient: ClientGrpc,
+    @Inject('CUSTOM_HELLO_SERVICE') private customHelloClient: ClientGrpc,
+  ) {}
+
+  onModuleInit() {
+    this.helloWorldService = this.helloWorldClient.getService<
+      HelloWorldService
+    >('HelloWorldService');
+    this.customHelloService = this.customHelloClient.getService<
+      CustomHelloService
+    >('CustomHelloService');
+  }
+
+  getHelloWorld() {
+    return this.helloWorldService.getHelloWorld({});
+  }
+
+  getCustomHello(data: string) {
+    return this.customHelloService.getCustomHello({ data });
   }
 }
